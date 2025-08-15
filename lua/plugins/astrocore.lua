ac43d2ece5a493eb67afd6a0c9ddc56625dbcd85
buildrobotsh <moosboos1@gmail.com>
2025-08-15 11:20:26 -0400

    ui preference tweaks

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -40,8 +40,8 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
+        relativenumber = false, -- sets vim.opt.relativenumber
+        number = false, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
